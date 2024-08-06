3d5f6e29ae2ac59b76b7a1b89ca60bd69fb3be00
Max-YUAN <149166590+MAX-YUQI@users.noreply.github.com>
2024-08-06 12:54:41 +0800

    Update P5732.c

--- 代码库/P5732.c
+++ 代码库/P5732.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 void generateYanghuiTriangle(int n, int triangle[][n]) {
-    // 初始化第一行
+
     triangle[0][0] = 1;
     for (int i = 1; i < n; i++) {
-        // 每一行的第一个和最后一个数字都是1
+
         triangle[i][0] = 1;
         triangle[i][i] = 1;
-        // 填充中间的数字
+
         for (int j = 1; j < i; j++) {
             triangle[i][j] = triangle[i-1][j-1] + triangle[i-1][j];
         }
@@ -27,13 +27,10 @@ int main() {
     int n;
     scanf("%d", &n);
 
-    // 声明一个二维数组存储杨辉三角
     int triangle[n][n];
 
-    // 生成杨辉三角
     generateYanghuiTriangle(n, triangle);
 
-    // 输出杨辉三角
     printYanghuiTriangle(n, triangle);
 
     return 0;
