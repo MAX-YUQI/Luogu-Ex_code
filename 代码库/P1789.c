3939b089ddaf3cf6e688f12acbdaee156926a292
Max-YUAN <149166590+MAX-YUQI@users.noreply.github.com>
2024-08-06 13:00:46 +0800

    Update P1789.c

--- 代码库/P1789.c
+++ 代码库/P1789.c
@@ -2,7 +2,6 @@
 #include<stdlib.h>
 
 void Huoba(int n, int grid[n][n], int x, int y) {
-    // 火把照亮范围
     for (int i = -2; i <= 2; i++) {
         for (int j = -2; j <= 2; j++) {
             if (x + i >= 0 && x + i < n && y + j >= 0 && y + j < n) {
@@ -15,7 +14,6 @@ void Huoba(int n, int grid[n][n], int x, int y) {
 }
 
 void Yingshi(int n, int grid[n][n], int x, int y) {
-    // 萤石照亮范围
     for (int i = -2; i <= 2; i++) {
         for (int j = -2; j <= 2; j++) {
             if (x + i >= 0 && x + i < n && y + j >= 0 && y + j < n) {
@@ -30,28 +28,24 @@ int main() {
     scanf("%d %d %d", &n, &m, &k);
 
     int grid[n][n];
-    // 初始化grid为0，表示未被照亮
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
             grid[i][j] = 0;
         }
     }
 
-    // 读取火把位置并照亮
     for (int i = 0; i < m; i++) {
         int x, y;
         scanf("%d %d", &x, &y);
         Huoba(n, grid, x - 1, y - 1);
     }
 
-    // 读取萤石位置并照亮
     for (int i = 0; i < k; i++) {
         int x, y;
         scanf("%d %d", &x, &y);
         Yingshi(n, grid, x - 1, y - 1);
     }
 
-    // 统计未被照亮的方格
     int darkCount = 0;
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
