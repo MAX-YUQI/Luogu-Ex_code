5644863172b3db1652d660bb1acb3ed75e6c9949
Max-YUAN <149166590+MAX-YUQI@users.noreply.github.com>
2024-08-06 12:57:49 +0800

    Update P2669.c

--- 代码库/P2669.c
+++ 代码库/P2669.c
@@ -6,19 +6,16 @@ int main() {
     scanf("%d", &k);  
   
     while (days < k) {  
-        // 计算当前周期的金币总数  
         current_sum = n * n;  
   
-        // 如果加上当前周期的金币数会超过k，则只累加部分天数  
         if (days + n > k) {  
             total += (k - days) * n;  
             break;  
         }  
-  
-        // 否则，累加整个周期的金币数  
+
         total += current_sum;  
         days += n;  
-        n++; // 进入下一个周期  
+        n++;
     }  
   
     printf("%d\n", total);  
